717eabf8fbfa4b6027d3478206c1a3514a8ccb96
Victor Mutiso <mutisovictor69@gmail.com>
2024-04-15 20:59:01 +0300

    Christ is King

---
+++ PyShop/products/apps.py
@@ -0,0 +1,9 @@
+import django.db.models
+from django.apps import AppConfig
+
+
+class ProductsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "products"
+
+
---
+++ PyShop/products/urls.py
@@ -0,0 +1,9 @@
+import http
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index),
+    path("new", views.new)
+]
